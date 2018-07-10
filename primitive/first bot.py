dd40a7e9e30a23ddca213d14ddf7c4cdfb6f7bd8
KVR Aditya <kvraditya577@gmail.com>
2018-07-10 17:11:02 +0530

    This my first attempt to make the chatbox

---
+++ primitive/first bot.py
@@ -0,0 +1,17 @@
+from chatterbot.trainers import ListTrainer
+#this is actually the method used to train the chatbox
+from chatterbot import ChatBot
+
+print("Bot: Welcome to the Coustomer Care Service of XXXXXX Company.May I know your predicament")
+bot= ChatBot('Test')
+
+conv = open('chats.txt','r').readlines()
+bot.set_trainer(ListTrainer)
+#Sets the train
+bot.train(conv)
+while True:
+    request = input('You: ')
+    response = bot.get_response(request)
+    print('Bot: ',response)
+
+print("HAve a good day")
